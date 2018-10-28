59baf5c25e5f65074808b293065e751484e1b8eb
Jonathan Gresl <43157234+jgresl@users.noreply.github.com>
2018-10-28 15:03:48 -0700

    Add files via upload

--- ZooBC - DDL.sql
+++ ZooBC - DDL.sql
@@ -1,21 +1,46 @@
+DROP TABLE IF EXISTS UserAccount;
+DROP TABLE IF EXISTS Animal;
+
 
 CREATE TABLE User (
 	User_ID INTEGER,
-	userEmail VARCHAR(30),
-	userPassWord VARCHAR(20),
-	userType VARCHAR(10),
-	firstName VARCHAR(20),
-	lastName VARCHAR(20),
-	phoneNumber INTEGER,
-	address VARCHAR(80),
-	country VARCHAR(20),
-	stateProvince CHAR(2),
-	zipPostal VARCHAR(10),
-	city VARCHAR(20),
-	streetName VARCHAR(20),
-	streetNumber INTEGER,
+	userEmail VARCHAR(30) NOT NULL,
+	userPassword VARCHAR(20) NOT NULL,
+	isAdmin BOOLEAN, 
+	firstName VARCHAR(20) NOT NULL,
+	lastName VARCHAR(20) NOT NULL,
+	phoneNumber INTEGER NOT NULL,
+	address VARCHAR(80) NOT NULL,
+	country VARCHAR(20) NOT NULL,
+	stateProvince CHAR(2) NOT NULL,
+	zipPostal VARCHAR(10) NOT NULL,
+	city VARCHAR(20) NOT NULL,
+	streetName VARCHAR(20) NOT NULL,
+	streetNumber INTEGER NOT NULL,
 	aptNumber INTEGER,
-	PRIMARY KEY User_ID,
-	FOREIGN KEY cart_ID REFERENCES ShoppingCart(cart_ID)
-{
+	PRIMARY KEY (User_ID),
+	FOREIGN KEY (cart_ID) REFERENCES ShoppingCart(cart_ID),
+	UNIQUE (userEmail),
+);
+
+	CREATE TABLE Animal (
+	animal_ID INTEGER,
+	animalName VARCHAR(20) NOT NULL,
+	animalType VARCHAR(10) NOT NULL,
+	animalGender VARCHAR(10) NOT NULL,
+	animalSize VARCHAR(10) NOT NULL,
+	animalTemper VARCHAR(10) NOT NULL,
+	animalStatus VARCHAR(20) NOT NULL,
+	animalPrice CURRENCY NOT NULL,
+	description VARCHAR(255) NOT NULL,
+	image BLOB,
+	PRIMARY KEY (animal_ID)
+);
+
+
+
+
+
+
+
 
