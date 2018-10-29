9690708e56d6a2a653507e74bb846c450f8a19ce
Jonathan Gresl <43157234+jgresl@users.noreply.github.com>
2018-10-29 16:31:23 -0700

    Add files via upload

--- ZooBC - DDL.sql
+++ ZooBC - DDL.sql
@@ -10,7 +10,7 @@ DROP TABLE IF EXISTS Reviews;
 DROP TABLE IF EXISTS Stores;
 
 CREATE TABLE UserAccount (
-	User_ID INTEGER,
+	User_ID INTEGER AUTO_INCREMENT,
 	userEmail VARCHAR(30) NOT NULL,
 	userPassword VARCHAR(20) NOT NULL,
 	isAdmin BOOLEAN, 
@@ -23,15 +23,15 @@ CREATE TABLE UserAccount (
 	zipPostal VARCHAR(10) NOT NULL,
 	city VARCHAR(20) NOT NULL,
 	streetName VARCHAR(20) NOT NULL,
-	streetNumber INTEGER NOT NULL,
-	aptNumber INTEGER,
+	streetNumber VARCHAR(10) NOT NULL,
+	aptNumber VARCHAR(10),
 	PRIMARY KEY (User_ID),
 	FOREIGN KEY (cart_ID) REFERENCES ShoppingCart(cart_ID),
 	UNIQUE (userEmail),
 );
 
 CREATE TABLE Animal (
-	animal_ID INTEGER,
+	animal_ID INTEGER AUTO_INCREMENT,
 	animalName VARCHAR(20) NOT NULL,
 	animalType VARCHAR(10) NOT NULL,
 	animalGender VARCHAR(10) NOT NULL,
@@ -45,8 +45,9 @@ CREATE TABLE Animal (
 );
 
 CREATE TABLE CreditCard (
-	card_ID INTEGER,
+	card_ID INTEGER AUTO_INCREMENT,
 	cardType VARCHAR(10) NOT NULL,
+	cardName VARCHAR(10) NOT NULL,
 	cardNumber CHAR(16) NOT NULL,
 	cardExpiration CHAR(4) NOT NULL,
 	cardCCV CHAR(3) NOT NULL,
@@ -56,21 +57,21 @@ CREATE TABLE CreditCard (
 	zipPostal VARCHAR(10) NOT NULL,
 	city VARCHAR(20) NOT NULL,
 	streetName VARCHAR(20) NOT NULL,
-	streetNumber INTEGER NOT NULL,
-	aptNumber INTEGER,
-	PRIMARY KEY (card_ID)
+	streetNumber VARCHAR(10) NOT NULL,
+	aptNumber VARCHAR(10),
+	PRIMARY KEY (card_ID),
 );
 
 CREATE TABLE Warehouse (
-	warehouse_ID INTEGER,
+	warehouse_ID INTEGER AUTO_INCREMENT,
 	continent VARCHAR(20),
 	country VARCHAR(20),
 	PRIMARY KEY (warehouse_ID)
 );
 
 CREATE TABLE OrderShipment (
-	order_ID INTEGER,
-	orderDate DATETIME NOT NULL,
+	order_ID INTEGER AUTO_INCREMENT,
+	orderDate TIMESTAMP NOT NULL,
 	orderStatus VARCHAR(10) NOT NULL,
 	subTotal CURRENCY,
 	shippingCost CURRENCY,
@@ -86,21 +87,27 @@ CREATE TABLE OrderShipment (
 	b_zipPostal VARCHAR(10) NOT NULL,
 	b_city VARCHAR(20) NOT NULL,
 	b_streetName VARCHAR(20) NOT NULL,
-	b_streetNumber INTEGER NOT NULL,
-	b_aptNumber INTEGER,
+	b_streetNumber VARCHAR(10) NOT NULL,
+	b_aptNumber VARCHAR(10),
 	shippingAddress VARCHAR(80) NOT NULL,
 	s_country VARCHAR(20) NOT NULL,
 	s_stateProvince CHAR(2) NOT NULL,
 	s_zipPostal VARCHAR(10) NOT NULL,
 	s_city VARCHAR(20) NOT NULL,
 	s_streetName VARCHAR(20) NOT NULL,
-	s_streetNumber INTEGER NOT NULL,
-	s_aptNumber INTEGER,
-	PRIMARY KEY (order_ID)
+	s_streetNumber VARCHAR(10) NOT NULL,
+	s_aptNumber VARCHAR(10),
+	warehouse_ID INTEGER,
+	shipping_ID INTEGER,
+	card_ID INTEGER,
+	PRIMARY KEY (order_ID),
+	FOREIGN KEY warehouse_ID REFERENCES Warehouse(warehouse_ID),
+	FOREIGN KEY shipping_ID REFERENCES ShippingOption(shipping_ID),
+	FOREIGN KEY card_ID REFERENCES CreditCard(card_ID)
 );
 
 CREATE TABLE ShippingOption (
-	shipping_ID INTEGER,
+	shipping_ID INTEGER AUTO_INCREMENT,
 	shipMethod VARCHAR(10) NOT NULL,
 	shipPrice CURRENCY NOT NULL,
 	PRIMARY KEY (shipping_ID)
@@ -114,19 +121,33 @@ CREATE TABLE TaxOption (
 );
 
 CREATE TABLE ShoppingCart (
-	cart_ID INTEGER,
-	quantity INTEGER,
-	PRIMARY KEY (cart_ID)
+	user_ID INTEGER,
+	animal_ID INTEGER,
+	cartDate TIMESTAMP,
+	cartQuantity INTEGER,
+	PRIMARY KEY (user_ID)
 );
 
 CREATE TABLE Reviews (
-	dateReviewed DATETIME,
-	overallRating INTEGER [1..5],
-	review VARCHAR(255)
-	PRIMARY KEY ( , )
+	user_ID INTEGER,
+	animal_ID INTEGER,
+	dateReviewed TIMESTAMP,
+	overallRating INTEGER,
+	review VARCHAR(255),
+	CHECK (overallRating >= 1 AND overallRating <= 5),
+	PRIMARY KEY (user_ID, animal_ID)
 );
 
 CREATE TABLE Stores (
+	warehouse_ID INTEGER,
+	animal_ID INTERFACE,
 	inventory INTEGER,
-	PRIMARY KEY ( , )
+	PRIMARY KEY (warehouse_ID, animal_ID)
 );
+
+CREATE TABLE SavesCard (
+	user_ID INTEGER,
+	card_ID INTEGER,
+	PRIMARY KEY (user_ID, card_ID)
+);
+	
